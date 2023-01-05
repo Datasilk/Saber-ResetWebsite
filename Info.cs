bf88cf6f71482f55030c0586d1ebff3c38aa1418
Mark Entingh <entingh@gmail.com>
2023-01-05 00:27:54 -0600

    fixed ambiguious name

--- Info.cs
+++ Info.cs
@@ -8,6 +8,6 @@ namespace Saber.Vendors.ResetWebsite
         public string Name { get; set; } = "Reset Website";
         public string Description { get; set; } = "Allows administrators to reset their current website to the default template website.";
         public string Icon { get; set; }
-        public Version Version { get; set; } = "1.0.0.0";
+        public Vendor.Version Version { get; set; } = "1.0.0.0";
     }
 }
